4184bff23272e4ffe48b6ca516b1c66dec26a198
Andrew Backes <backes.andrew@gmail.com>
2021-02-12 10:58:00 -0600

    Add V2 data, remove V1

--- README.md
+++ README.md
@@ -8,6 +8,24 @@ This is the data found within the [Pokémon TCG API](https://pokemontcg.io/). Cu
 
 The easiest way to stay up to date and interact with the data is via the [Pokémon TCG API](http://pokemontcg.io/) and one of the associated SDKs. Otherwise, feel free to clone this repository or download a zip from the releases.
 
+# Version 1 and 2 Data
+
+Version 1 data is no longer being maintained. The API for V1 will continue to receive new sets until August 1st, 2021. At this time, V1 of the API will be taken offline, and you MUST be using V2. You have a 6 month window to migrate to V2.
+
+If you rely on the V1 data, I have provided a `v2_to_v1.rb` Ruby script that you can run to generate all the json files in v1 format.
+
+To install Ruby: https://www.ruby-lang.org/en/documentation/installation/
+
+You will also need the `json` gem: `gem install json`.
+
+Finally, to run the script:
+
+```
+ruby v2_to_v1.rb
+```
+
+This will output all of the card data into `/cards/en/v1`.
+
 # Contributing
 
 Please contribute when you see missing and/or incorrect data. I'll try to review all pull requests relatively quickly so that I can push updates at night.
---
+++ v2_to_v1.rb
@@ -0,0 +1,51 @@
+require 'json'
+
+class Hash
+  def compact
+    delete_if{|k, v| v.nil? || (v.kind_of?(String) && v.empty?) || (v.kind_of?(Array) && !v.any?) || (v.instance_of?(Hash) && v.compact.empty?)}
+  end
+end
+
+def sorted_hash(input_hash, key_sort_order)
+  new_hash = {}
+  key_sort_order.each do |key|
+    if input_hash.has_key?(key)
+      new_hash[key] = input_hash[key]
+    end
+  end
+  new_hash
+end
+
+sort_order = [:id, :name, :imageUrl, :subtype, :supertype, :level, :evolvesFrom, :ability, :ancientTrait, :hp, :retreatCost,
+  :convertedRetreatCost, :number, :artist, :rarity, :series, :set, :setCode, :text, :types, :attacks, :weaknesses,
+  :resistances, :imageUrlHiRes, :nationalPokedexNumber, :evolvesTo
+]
+
+sets = JSON.parse(File.open('sets/en.json').read, :symbolize_names => true)
+
+Dir.glob('cards/en/*.json') do |json_file|
+  puts "Converting #{File.basename json_file}..."
+  cards_file = File.open(json_file, "r:UTF-8")
+  data = cards_file.read
+  cards = JSON.parse(data, :symbolize_names => true)
+  set = sets.select{|x| x[:id] == File.basename(json_file, '.json')}.first
+
+  v1_cards = []
+  cards.each do |card|
+    card[:imageUrl] = card[:images][:small]
+    card[:subtype] = card[:subtypes].last unless card[:abilities].nil?
+    card[:ability] = card[:abilities].first unless card[:abilities].nil?
+    card[:series] = set[:series]
+    card[:setCode] = set[:id]
+    card[:text] = card[:rules]
+    card[:imageUrlHiRes] = card[:images][:large]
+    card[:nationalPokedexNumber] = card[:nationalPokedexNumbers].first unless card[:nationalPokedexNumbers].nil?
+
+    v1_cards.push(sorted_hash(card.compact, sort_order))
+  end
+
+  output_dir = "cards/en/v1"
+  Dir.mkdir(output_dir) unless File.exists?(output_dir)
+  File.open("#{output_dir}/#{File.basename json_file}", 'w+') { |file| file.write(JSON.pretty_generate(v1_cards)) }
+end
+
