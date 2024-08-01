2b53355e3478b1cca2bb02397257be7eaf28cd83
Muculus478 <esevart@gmail.com>
2024-07-31 21:37:19 -0400

    Update ImportSelenium.ps1

--- scripts/ImportSelenium.ps1
+++ scripts/ImportSelenium.ps1
@@ -7,24 +7,24 @@ Import-Module Selenium
 # Start a new browser instance
 $driver = Start-SeChrome
 
-# Navigate to the URL
-$driver.Navigate().GoToUrl("https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660")
-
-# Give the page some time to load
-Start-Sleep -Seconds 5
-
-# Find the <textarea> element by XPath
-$textarea = $driver.FindElementByXPath("/html/body/main/div/div[2]/form/textarea")
-
-# Extract and print the content
-if ($textarea -ne $null) {
-    $content = $textarea.Text.Trim()
-    Write-Output "Content of the <textarea>:"
-    Write-Output $content
-} else {
-    Write-Output "No <textarea> element found with the specified XPath."
-}
-
-# Close the browser
-$driver.Quit()
+## Navigate to the URL
+#$driver.Navigate().GoToUrl("https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660")
+#
+## Give the page some time to load
+#Start-Sleep -Seconds 5
+#
+## Find the <textarea> element by XPath
+#$textarea = $driver.FindElementByXPath("/html/body/main/div/div[2]/form/textarea/text()")
+#
+## Extract and print the content
+#if ($textarea -ne $null) {
+#    $content = $textarea.Text.Trim()
+#    Write-Output "Content of the <textarea>:"
+#    Write-Output $content
+#} else {
+#    Write-Output "No <textarea> element found with the specified XPath."
+#}
+#
+## Close the browser
+#$driver.Quit()
 
