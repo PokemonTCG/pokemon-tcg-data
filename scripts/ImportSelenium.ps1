f339209c63d4f7d23c3cf6f883a243e92ac21a3c
Muculus478 <esevart@gmail.com>
2024-08-03 21:18:02 -0400

    Changed Made in testing

--- scripts/ImportSelenium.ps1
+++ scripts/ImportSelenium.ps1
@@ -1,47 +1,6 @@
-# PowerShell script to fetch raw contents of a webpage using Selenium
-
-# Variables for file paths
-$seleniumPath = "C:\Users\Gunna\Downloads\selenium-java-4.23.0"
-$javaPath = "C:\Program Files\Java\jre-1.8\bin\java.exe"
-$chromeDriverPath = "C:\Program Files\WindowsPowerShell\Modules\Selenium\3.0.1\assemblies\linux\chromedriver"
-
-# Java classpath setup for Selenium
-$classpath = "$seleniumPath\client-combined-4.23.0.jar;$seleniumPath\client-combined-4.23.0-nodeps.jar;$seleniumPath\libs\*"
-
-# Java code to perform the web scraping
-$javaCode = @"
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import java.nio.file.*;
-import java.io.*;
-
-public class WebScraper {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "$chromeDriverPath");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660");
-        
-        // Get the page source (raw HTML content)
-        String pageSource = driver.getPageSource();
-        
-        // Save the page source to a file
-        try {
-            Files.write(Paths.get("pageSource.html"), pageSource.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        driver.quit();
-    }
-}
-"@
-
-# Save the Java code to a file
-$javaFilePath = "C:\Users\Gunna\Downloads\WebScraper.java"
-Set-Content -Path $javaFilePath -Value $javaCode
-
-# Compile the Java code
-& $javaPath\javac.exe -cp $classpath $javaFilePath
-
-# Run the compiled Java program
-& $javaPath\java.exe -cp "$classpath;C:\Users\Gunna\Downloads" WebScraper
+OK so the file path for Selenium Java Download is here. C:\Users\Gunna\Downloads\selenium-java-4.23.0   
+please create powershell script to use selenium to get RAW contents of this website. https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660. Here
+ is my Java file path"C:\Program Files\Java\jre-1.8\bin\java.exe"  And here is the path where JDK lives OK so the file path for 
+Selenium Java Download is here. C:\Users\Gunna\Downloads\selenium-java-4.23.0   please create powershell script to use 
+selenium to get RAW contents of this website. https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660. 
+Here is my Java file path"C:\Program Files\Java\jre-1.8\bin\java.exe"
--- scripts/OpenURL.ps1
+++ scripts/OpenURL.ps1
@@ -1,3 +1,53 @@
 # Specify the URL you want to open
-$URL = "https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660"
-curl $URL
+#$URL = "https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660"
+#curl $URL
+#Add-Type -AssemblyName System.Windows.Forms
+#Add-Type -AssemblyName System.Drawing
+
+# Create the form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Select a URL"
+$form.Size = New-Object System.Drawing.Size(400, 250)
+$form.StartPosition = "CenterScreen"
+
+# Create radio buttons
+$radioButton1 = New-Object System.Windows.Forms.RadioButton
+$radioButton1.Location = New-Object System.Drawing.Point(20, 30)
+$radioButton1.Size = New-Object System.Drawing.Size(350, 20)
+$radioButton1.Text = "Muddy Waters - SGLC Semi Gym Leader Challenge"
+$radioButton1.Tag = "https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660"
+$form.Controls.Add($radioButton1)
+
+$radioButton2 = New-Object System.Windows.Forms.RadioButton
+$radioButton2.Location = New-Object System.Drawing.Point(20, 60)
+$radioButton2.Size = New-Object System.Drawing.Size(350, 20)
+$radioButton2.Text = "Dark Cycle - SGLC Semi Gym Leader Challenge"
+$radioButton2.Tag = "https://pokemoncard.io/deck/dark-cycle-sglc-semi-gym-leader-challenge-35671"
+$form.Controls.Add($radioButton2)
+
+$radioButton3 = New-Object System.Windows.Forms.RadioButton
+$radioButton3.Location = New-Object System.Drawing.Point(20, 90)
+$radioButton3.Size = New-Object System.Drawing.Size(350, 20)
+$radioButton3.Text = "Rainbow Deck - SGLC Semi Gym Leader Challenge"
+$radioButton3.Tag = "https://pokemoncard.io/deck/rainbow-deck-sglc-semi-gym-leader-challenge-35669"
+$form.Controls.Add($radioButton3)
+
+# Create a button
+$button = New-Object System.Windows.Forms.Button
+$button.Location = New-Object System.Drawing.Point(150, 150)
+$button.Size = New-Object System.Drawing.Size(100, 30)
+$button.Text = "Go"
+$form.Controls.Add($button)
+
+# Button click event
+$button.Add_Click({
+    $selectedRadioButton = $form.Controls | Where-Object { $_.Checked -eq $true }
+    if ($selectedRadioButton) {
+        Start-Process $selectedRadioButton.Tag
+    } else {
+        [System.Windows.Forms.MessageBox]::Show("Please select a URL.", "Warning")
+    }
+})
+
+# Show the form
+[void] $form.ShowDialog()
