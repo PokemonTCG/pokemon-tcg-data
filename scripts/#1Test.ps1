f79009e6cf8ffc827ef1d45e32f5291fa9d55fd6
Muculus478 <esevart@gmail.com>
2024-08-03 21:24:29 -0400

    Added Muddy Waters
    Deleted Bad Scripts

--- scripts/#1Test.ps1
+++
@@ -1,6 +0,0 @@
-Add-Type -Path "C:\Program Files\PackageManagement\NuGet\Packages\HtmlAgilityPack.1.11.61\lib\NetCore45\HtmlAgilityPack.dll"
-if (-not [System.Reflection.Assembly]::LoadFile("C:\Program Files\PackageManagement\NuGet\Packages\HtmlAgilityPack.1.11.61\lib\NetCore45\HtmlAgilityPack.dll")) {
-    Write-Output "Failed to load HtmlAgilityPack.dll"
-} else {
-    Write-Output "HtmlAgilityPack.dll loaded successfully"
-}
--- scripts/#3Test.ps1
+++
@@ -1,7 +0,0 @@
-$textarea = $page.DocumentNode.SelectSingleNode("//textarea[@class='export-deck-list-list']")
-
-if ($textarea -ne $null) {
-    Write-Output "Found <textarea> element"
-} else {
-    Write-Output "No <textarea> element with class 'export-deck-list-list' found"
-}
